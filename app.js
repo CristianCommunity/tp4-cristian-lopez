a4a2112f1072a1eeaa85bdc0a5c046f766580a64
CristianLopez <cristianlopez.community@gmail.com>
2023-06-12 12:20:33 -0300

    Configuración de los middlewares

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ const helmet = require("helmet");
 const app = express();
 const port = 6000;
 
+//Configuración de los middlewares
 app.use(morgan('dev'));
 
+app.use(helmet());
+
+app.use(cors());
+
+app.use(express.json());
+
+//Relaciona las rutas con las respuestas
 app.use(require('./routes/app.routes'));
 
 app.listen(port, () => {
     console.log(`Escuchando el puerto ${port}`)
 })
+
 
